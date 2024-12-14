2acd5dd51b006ded2b9fbc14ed846d23fd5e4cce
arielbaeeeee <ariel.cpyue0227@gmail.com>
2024-12-14 17:53:23 +1100

    Add files via upload

---
+++ 35. Search Insert Position/searchInsert.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (target < nums[mid]) {
+                right = mid - 1;
+            }
+            else if (target > nums[mid]) {
+                left = mid + 1;
+            }
+            else {
+                return mid;
+            }
+        }
+        return right + 1;
+    }
+}
